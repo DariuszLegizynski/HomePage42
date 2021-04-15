59c004cfb86955e90bfe7830e04899d1d89f503b
Dariusz Legizynski <dariuszlegizynski@Dariuszs-MacBook-Pro.local>
2021-04-15 16:56:23 +0200

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+	<header class="header">
+		<img src="#" alt="logo" />
+		<span class="header__burger">BURGER</span>
+		<nav class="header__nav">
+			<ul>
+				<li class="header__list-item">Home</li>
+				<li class="header__list-item">About Us</li>
+				<li class="header__list-item">CMS & Web</li>
+				<li class="header__list-item">E-Commerce</li>
+				<li class="header__list-item">Apps & Mobile</li>
+				<li class="header__list-item">Full Service</li>
+			</ul>
+		</nav>
+	</header>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
