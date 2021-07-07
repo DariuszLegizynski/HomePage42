220a6643bcc871cea2948b7765c538914bfca5b7
Dariusz Legizynski <dariuszlegizynski@lemon42.com>
2021-07-07 23:42:54 +0200

    add parallax

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -1,54 +1,48 @@
 <template>
-  <!-- <section class="hero">
-    <div class="hero-parallax">
-      <div class="hero-parallax__layer hero-parallax__layer--back">
-        <h2 class="hero__h2">We are the ANSWER</h2>
-      </div>
-      <div class="hero-parallax__layer hero-parallax__layer--base">
-        <h5 class="hero__h5">What, why?!</h5>
-      </div>
-    </div>
-  </section> -->
   <section class="hero">
-    <div class="rellax" data-rellax-speed="-6">
+    <ScrollParallax class="hero__leaf1" direction="y" :speed="0.8">
       <img src="~@/assets/pics/leaves/leaf1.png" alt="leaf1" />
-    </div>
-    <div class="rellax" data-rellax-speed="-5">
+    </ScrollParallax>
+    <ScrollParallax class="hero__leaf2" direction="y" :speed="0.7">
       <img src="~@/assets/pics/leaves/leaf2.png" alt="leaf2" />
-    </div>
-    <div class="rellax" data-rellax-speed="-4">
+    </ScrollParallax>
+    <ScrollParallax class="hero__leaf3" direction="y" :speed="0.6">
       <img src="~@/assets/pics/leaves/leaf3.png" alt="leaf3" />
-    </div>
-    <div class="rellax" data-rellax-speed="-3">
-      <h2 class="hero__h2">We are the ANSWER</h2>
-    </div>
-    <div class="rellax">
+    </ScrollParallax>
+    <ScrollParallax class="hero__leaf4" direction="y" :speed="0.3">
       <img src="~@/assets/pics/leaves/leaf4.png" alt="leaf4" />
-    </div>
-    <div class="rellax" data-rellax-speed="1">
+    </ScrollParallax>
+    <ScrollParallax class="hero__leaf5" direction="y" :speed="0.5">
       <img src="~@/assets/pics/leaves/leaf6.png" alt="leaf6" />
-    </div>
-    <div class="rellax" data-rellax-speed="2">
+    </ScrollParallax>
+    <ScrollParallax class="hero__leaf6" direction="y" :speed="0.1">
+      <h2 class="hero__h2">We are the ANSWER</h2>
+    </ScrollParallax>
+    <ScrollParallax class="hero__leaf7" direction="y" :speed="0.3">
       <img src="~@/assets/pics/leaves/leaf7.png" alt="leaf7" />
-    </div>
-    <div class="rellax" data-rellax-speed="3">
+    </ScrollParallax>
+    <ScrollParallax class="hero__leaf8" direction="y" :speed="0.01">
       <img src="~@/assets/pics/leaves/leaf5.png" alt="leaf5" c />
-    </div>
+    </ScrollParallax>
   </section>
 </template>
 
 <script>
-export default {}
+import ScrollParallax from 'vue3-parallax/src/components/ScrollParallax.vue'
+export default {
+  components: {
+    ScrollParallax
+  }
+}
 </script>
 
 <style scoped>
 .hero {
-  height: 100%;
-  width: 100%;
-  position: relative;
-  overflow: hidden;
   background: url(~@/assets/pics/mae-mu-YdN-kUtnOEQ-unsplash.jpeg) no-repeat
-    center center fixed;
-  background-size: cover;
+    center center/cover fixed;
+  height: 100vh;
+}
+.hero img {
+  height: 4rem;
 }
 </style>
