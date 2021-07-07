9c535446bd8ffc10a217e285379e2b3d839be7bf
Dariusz Legizynski <dariuszlegizynski@lemon42.com>
2021-07-07 23:49:55 +0200

    spread the leaves with p:absolute

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -12,17 +12,17 @@
     <ScrollParallax class="hero__leaf4" direction="y" :speed="0.3">
       <img src="~@/assets/pics/leaves/leaf4.png" alt="leaf4" />
     </ScrollParallax>
-    <ScrollParallax class="hero__leaf5" direction="y" :speed="0.5">
-      <img src="~@/assets/pics/leaves/leaf6.png" alt="leaf6" />
+    <ScrollParallax class="hero__leaf5" direction="y" :speed="0.4">
+      <img src="~@/assets/pics/leaves/leaf5.png" alt="leaf5" c />
     </ScrollParallax>
-    <ScrollParallax class="hero__leaf6" direction="y" :speed="0.1">
+    <ScrollParallax class="hero__title" direction="y" :speed="0.1">
       <h2 class="hero__h2">We are the ANSWER</h2>
     </ScrollParallax>
-    <ScrollParallax class="hero__leaf7" direction="y" :speed="0.3">
-      <img src="~@/assets/pics/leaves/leaf7.png" alt="leaf7" />
+    <ScrollParallax class="hero__leaf6" direction="y" :speed="0.5">
+      <img src="~@/assets/pics/leaves/leaf6.png" alt="leaf6" />
     </ScrollParallax>
-    <ScrollParallax class="hero__leaf8" direction="y" :speed="0.01">
-      <img src="~@/assets/pics/leaves/leaf5.png" alt="leaf5" c />
+    <ScrollParallax class="hero__leaf7" direction="y" :speed="0.01">
+      <img src="~@/assets/pics/leaves/leaf7.png" alt="leaf7" />
     </ScrollParallax>
   </section>
 </template>
@@ -41,8 +41,44 @@ export default {
   background: url(~@/assets/pics/mae-mu-YdN-kUtnOEQ-unsplash.jpeg) no-repeat
     center center/cover fixed;
   height: 100vh;
+  position: relative;
 }
 .hero img {
   height: 4rem;
+}
+.hero div {
+  position: absolute;
+}
+.hero__leaf1 {
+  top: 4rem;
+  left: 2rem;
+}
+.hero__leaf2 {
+  top: 8rem;
+  left: 4rem;
+}
+.hero__leaf3 {
+  top: 14rem;
+  left: 12rem;
+}
+.hero__leaf4 {
+  top: 11rem;
+  right: 2rem;
+}
+.hero__leaf5 {
+  bottom: 13rem;
+  left: 14rem;
+}
+.hero__leaf6 {
+  bottom: 9rem;
+  left: 7rem;
+}
+.hero__leaf7 {
+  bottom: 4rem;
+  right: 2rem;
+}
+.hero__title {
+  top: 20rem;
+  left: 10rem;
 }
 </style>
